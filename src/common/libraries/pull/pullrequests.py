4192960f3e1e830b39944dc0e1a248286946666e
Saurabh Pandey <spandey2405@gmail.com>
2016-06-02 12:37:05 +0530

    Debugging Process

--- src/common/libraries/pull/pullrequests.py
+++ src/common/libraries/pull/pullrequests.py
@@ -6,23 +6,22 @@ gitsys_settings = settings.GIT_SYNC_DIRECTORY
 class PullRequest():
 
     def execute(self, options):
-        print options
+        response = dict()
         selected_code_base = gitsys_settings[options['code_base']]
         dir = selected_code_base['dir']
         branch = selected_code_base['branch'][options['stage']]
         command = "cd {0} && git stash && git pull origin {1} && git stash pop".format(dir, branch)
         try:
-            response = run_command(command)
-            print response
+            response['pull_request'] = run_command(command)
         except Exception as e:
             print e
             response = False
-        self.runprocess(selected_code_base['process'])
+        response['process'] = self.runprocess(selected_code_base['process'])
         return response
 
 
     def runprocess(self, process):
-
+        response = dict()
         for event in process:
             event = event.split('.')
             process_name = event[0]
@@ -30,14 +29,13 @@ class PullRequest():
 
             module = process_info['module_name']
             type   = process_info['type']
-            print module, type
             if type == "SysVinit":
                 command = "Service {0} restart".format(module)
             else:
                 command = "restart {0}".format(module)
 
-            print command
+            response[process_name] = run_command(command)
 
-        return True
+        return response
 
 
